3f4767aa325b0db5d1b128a54b9c8e7fcb4f9631
ROEL2407 <roel24komen@gmail.com>
2022-04-28 15:26:04 +0200

    Update: retrieve current date

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4242;
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -12,16 +14,19 @@ const options = {
   day: 'numeric'
 }
 
+
 app.get("/", (req, res) => {
   let date = new Date();
   var allDates = [new Date().toLocaleDateString("nl-NL", options)];
   let length = 7;
-
+  
   for (let i = 0; i < length; i++) {
     date.setDate(date.getDate()+1);
     allDates.push(date.toLocaleDateString("nl-NL", options));
   }
-  res.render("index", {data: allDates});
+
+  const time = new Date().toLocaleTimeString("nl-NL", {hour: "numeric", minute: "numeric"})
+  res.render("index", {data: allDates, time: time});
 });
 
 app.listen(port, () => {
